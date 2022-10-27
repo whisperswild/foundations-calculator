fc14ee41fd99ca48a3d7c092dfc0e10ea13e06a0
Chris <Whisperswild@gmail.com>
2022-10-26 22:30:51 -0500

    * Set up initial files:     index.html, style.css, script.js *Set up initial file layouts:     index.html: basic div layouts and text     style.css:  created stylesheet entries     script.js:  initial basic functions set up

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(a, b){
+
+}
+function subtract(a, b){
+
+}
+function multiply(a, b){
+
+}
+function divide(a, b){
+
+}
+function operate(a, b, operator){
+
+}
+function clear(){
+
+}
+function display(input){
+
+}
+function history(log){
+
+}
+
+#inputbox{
+
+}
+
+#keypad{
+
+}
+
+#history{
+    
+}
