89243185b54c8fa47392504e3ed50819139c9b48
Chris <Whisperswild@gmail.com>
2022-11-05 23:04:35 -0500

    Add to an array each time a number key is pressed. Remove from the array when the backspace or delete keys are pressed. Clear the array and combine the numbers into a 'calculate array'     when an operator is pressed. Display the numbers in the input box on the calculator display. Fix logic for the keypress mouseclick events so presses aren't doubled.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const theAnswer = 0;
+const secondNumber = 0;
+const inputArray = [0];
+const calcArray = [0];
+const historyArray = [0];
 
 function add(a, b){
     return a + b;
@@ -29,7 +33,36 @@ function clear(){
 }
 
 function display(input){
-    //use this to create a div and fill it with the keyed input from the number keys
+
+    const inputBox = document.querySelector('#inputBox');
+    if(!isNaN(parseInt(input))){
+        inputArray[inputArray.length] = input;
+        const inputText = document.createTextNode(input);
+        inputBox.appendChild(inputText);
+    }else if (input === "Backspace" || input === "Delete"){
+        if(inputArray.length > 0){
+            inputArray.pop();
+        }
+        inputBox.innerHTML = "";
+        inputArray.forEach(i => {
+            inputText = document.createTextNode(i);
+            inputBox.appendChild(inputText);
+        })
+    }else if (input === "="){
+        inputArray.forEach(inputItem=>{
+            console.log(inputItem);
+        });
+    }else if (["+", "-", "*", "/"].includes(input)){
+        //operator found, store the current number and operator then reset the inputArray
+        //also clear out the inputbox and move it all to the calcbox
+        inputArray.forEach(i => {
+            calcArray[calcArray.length] += i;
+        });
+        calcArray[calcArray.length] = input;
+        
+    }else{
+        console.log(`Invalid key pressed: ${input}`);
+    }
 
 }
 
@@ -44,36 +77,37 @@ function btnClicked(btn, status){
         btn.classList.remove('clicked');
     }
 }
-
-    const div = document.querySelector('#keypad');
-
-    div.addEventListener('mousedown', (e) => {
+    //since we can get the target of the mouse click, we only need
+    //an eventlistener on the whole keypad instead of individual divs
+    const keyPad = document.querySelector('#keypad');
+    keyPad.addEventListener('mousedown', (e) => {
         btnClicked(e.target, 'clicked');
+        display(e.target.getAttribute('data-value'));
     });
-    div.addEventListener('mouseup', (e) => {
+    keyPad.addEventListener('mouseup', (e) => {
         btnClicked(e.target, 'unclicked');
     });
 
     //add a keyup/down event to capture 'Enter' and 'Backspace'
+    //convert 'enter' to '=' and handle backspace
 
+    //Loop through all the DIVs and find the one that matches the pressed key..
     const divs = document.querySelectorAll('div');
     const keypadPressDown = document.addEventListener('keydown', e=>{
         const keyPressed = e.key;
         divs.forEach(pressedNumKey => {
             if (pressedNumKey.getAttribute('data-value') === e.key){
                 btnClicked(pressedNumKey, 'clicked');
-                //console.log(`${pressedNumKey.getAttribute('data-value')} clicked!`);
+                display(e.key);
             }
         })
         
     })
-    const keypadPressUp = document.addEventListener('keyup', e=>{s
+    const keypadPressUp = document.addEventListener('keyup', e=>{
         const keyPressed = e.key;
         divs.forEach(pressedNumKey => {
             if (pressedNumKey.getAttribute('data-value') === e.key){
                 btnClicked(pressedNumKey, 'unclicked');
-                //console.log(`${pressedNumKey.getAttribute('data-value')} clicked!`);
             }
-        })
-        
+        })   
     })
