176bae5347ba7cf8a918988e801469d65018aeb0
Chris <Whisperswild@gmail.com>
2022-11-08 15:13:53 -0600

    Complete the logic for the calculator script Add some CSS for troubleshooting Update the HTML for troubleshooting

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-let theAnswer = 0;
-const inputLoop = 0;
-let inputArray = [0];
-let calcArray = [0];
-const historyArray = [0];
+//since we can get the target of the mouse click, we only need
+//an eventlistener on the whole keypad instead of individual divs
+const keyPad = document.querySelector('#keypad');
+const divs = document.querySelectorAll('div');
+
+keyPad.addEventListener('mousedown', (e) => {
+    btnClicked(e.target, 'clicked');
+    display(e.target.getAttribute('data-value'));
+});
+
+keyPad.addEventListener('mouseup', (e) => {
+    btnClicked(e.target, 'unclicked');
+});
+
+
+const keypadPressDown = document.addEventListener('keydown', e=>{
+    const keyPressed = e.key;
+    divs.forEach(pressedNumKey => {
+        if (pressedNumKey.getAttribute('data-value') === e.key){
+            btnClicked(pressedNumKey, 'clicked');
+            display(e.key);
+        }
+    })
+    
+});
+
+const keypadPressUp = document.addEventListener('keyup', e=>{
+    const keyPressed = e.key;
+    divs.forEach(pressedNumKey => {
+        if (pressedNumKey.getAttribute('data-value') === e.key){
+            btnClicked(pressedNumKey, 'unclicked');
+        }
+    })   
+});
 
 function add(a, b){
     return a + b;
@@ -24,71 +53,72 @@ function divide(a, b){
     }
     
 }
-function operate(a, b, operator){
 
+function operate(a, b, operator){
+    switch (operator){
+        case "+":
+            return add(a, b);
+        case "-": 
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
 }
+
+const OPERATORS = ["+", "-", "*", "/", "=", "(", ")"];
+let selectedOperator = '';
 
 function clear(){
-
+    historyBox.innerHTML = "";
+    calcBox.innerHTML = "";
+    inputBox.innerHTML = "";
+    selectedOperator = "";
 }
 
-function display(input){
-    
-    const inputBox = document.querySelector('#inputBox');
-    if(!isNaN(parseInt(input))){
-        inputArray[inputArray.length] = input;
-        historyArray[historyArray.length-1] = input;
-        const inputText = document.createTextNode(input);
-        inputBox.appendChild(inputText);
-    }else if (input === "Backspace" || input === "Delete"){
-        if(inputArray.length > 0){
-            inputArray.pop();
-            historyArray.pop();
-        }
-        inputBox.innerHTML = "";
-        inputArray.forEach(i => {
-            inputText = document.createTextNode(i);
-            inputBox.appendChild(inputText);
-        })
-    }else if (["+", "-", "*", "/", "="].includes(input)){
-        //operator found, store the current number and operator then reset the inputArray
-        //also clear out the inputbox and move it all to the calcbox
-        historyArray[historyArray.length-1] = input;
-        if (calcArray[0] === '' && inputArray.length > 0){
-            calcArray[0] = inputArray.join('');
-        }else if (calcArray[1] === ''){
-            calcArray[1] = inputArray.join('');
-        }else if (input === "="){
-            
-        }else{
-            inputArray = inputArray.join('');
+// first on each button press load the number into div
+// second, if operator button pressed, load div into a new variable from the div
+// second, clear the div (create copy in 'history' field?) 
+// repeat 
 
-            switch (input){
-                case "+":
-                    theAnswer = add(calcArray[0], inputArray[0]);
-                    break;
-                case "-":
-                    theAnswer = subtract(calcArray[0], inputArray[0]);
-                    break;
-                case "*":
-                    theAnswer = multiply(calcArray[0], inputArray[0]);
-                    break;
-                case "/":
-                    theAnswer = divide(calcArray[0], inputArray[0]);
-                    break;
+/* <div id="calcBox"></div>
+<div id="inputBox"></div>
+<div id="historyBox"></div> */
+const calcBox = document.querySelector('#calcBox');
+const inputBox = document.querySelector('#inputBox');
+const historyBox = document.querySelector('#historyBox');
+
+function display(input){
+    if (OPERATORS.includes(input)){
+        if(inputBox.innerHTML.length === 0){
+            console.log("Nothing to calculate against!");
+        }else if(calcBox.innerHTML.length === 0){
+            selectedOperator = input;
+            calcBox.innerHTML += inputBox.innerHTML;
+            historyBox.innerHTML += inputBox.innerHTML;
+            inputBox.innerHTML = "";
+        }else{
+            let result = operate(parseInt(calcBox.innerHTML), parseInt(inputBox.innerHTML), selectedOperator);
+            if (input === "="){
+                historyBox.innerHTML = result;
+            }else{
+                historyBox.innerHTML += input + inputBox.innerHTML;
             }
             
-            calcArray.length = 0;
-            inputArray.length = 0;
+            calcBox.innerHTML = result;
+            inputBox.innerHTML = "";
+
         }
     }else{
-        console.log(`Invalid key pressed: ${input}`);
+        inputBox.innerHTML += input;
     }
-
+    
+   
 }
 
 function history(log){
-//load the history into an array?
+
 }
 
 function btnClicked(btn, status){
@@ -98,37 +128,5 @@ function btnClicked(btn, status){
         btn.classList.remove('clicked');
     }
 }
-    //since we can get the target of the mouse click, we only need
-    //an eventlistener on the whole keypad instead of individual divs
-    const keyPad = document.querySelector('#keypad');
-    keyPad.addEventListener('mousedown', (e) => {
-        btnClicked(e.target, 'clicked');
-        display(e.target.getAttribute('data-value'));
-    });
-    keyPad.addEventListener('mouseup', (e) => {
-        btnClicked(e.target, 'unclicked');
-    });
 
-    //add a keyup/down event to capture 'Enter' and 'Backspace'
-    //convert 'enter' to '=' and handle backspace
 
-    //Loop through all the DIVs and find the one that matches the pressed key..
-    const divs = document.querySelectorAll('div');
-    const keypadPressDown = document.addEventListener('keydown', e=>{
-        const keyPressed = e.key;
-        divs.forEach(pressedNumKey => {
-            if (pressedNumKey.getAttribute('data-value') === e.key){
-                btnClicked(pressedNumKey, 'clicked');
-                display(e.key);
-            }
-        })
-        
-    })
-    const keypadPressUp = document.addEventListener('keyup', e=>{
-        const keyPressed = e.key;
-        divs.forEach(pressedNumKey => {
-            if (pressedNumKey.getAttribute('data-value') === e.key){
-                btnClicked(pressedNumKey, 'unclicked');
-            }
-        })   
-    })
